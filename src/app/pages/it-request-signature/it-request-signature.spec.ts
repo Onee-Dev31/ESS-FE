d2e3b81160040bcd20d15a4061da6c11eab08431
pung <pnugnolike@hotmail.com>
2026-03-02 15:57:46 +0700

    feat: add it-request-signature page with canvas signature pad

---
+++ src/app/pages/it-request-signature/it-request-signature.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ItRequestSignature } from './it-request-signature';
+
+describe('ItRequestSignature', () => {
+  let component: ItRequestSignature;
+  let fixture: ComponentFixture<ItRequestSignature>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ItRequestSignature]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ItRequestSignature);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
