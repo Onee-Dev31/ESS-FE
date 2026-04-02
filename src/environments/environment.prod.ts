c10cf721266a39fdfbaa42f4de11ebbb55cf2ac1
flukelnw1142 <73615503+flukelnw1142@users.noreply.github.com>
2026-04-02 15:59:21 +0700

    fix env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,7 @@
 export const environment = {
     production: true,
-    apiUrl: 'http://localhost:3000/api',
-    api_url: 'https://10.2.0.11:7081/api',
+    api_url: 'https://ess.oneeclick.co:7081/api',
     api_ONEEJOB_url: 'https://localhost:7113/api',
     emp_img_url: 'https://empimg.oneeclick.co:8048/employeeimage',
-    file_base_url: 'https://10.2.0.11:7081',
+    file_base_url: 'https://ess.oneeclick.co:7081',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'http://localhost:3000/api',
     api_url: 'http://localhost:5258/api',// http
     api_ONEEJOB_url: 'https://localhost:7113/api',
     // api_url: 'https://localhost:7081/api' //https
