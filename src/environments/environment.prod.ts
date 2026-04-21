96d4827abc5f9950e4b009ef001aadfe6d940b8e
Onee-Dev31 <dev@onee.one>
2026-04-21 17:46:41 +0700

    fix(environment): point api_url to production backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  api_url: '/api',
+  api_url: 'https://ess.oneeclick.co:7081/api',
   api_ONEEJOB_url: 'https://localhost:7113/api',
   emp_img_url: 'https://empimg.oneeclick.co:8048/employeeimage',
   file_base_url: '',
