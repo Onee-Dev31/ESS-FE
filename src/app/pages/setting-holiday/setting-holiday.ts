a3c3e9919a5aecead4f550b7292ee4bb7edb5d82
pure <140476156+pure11pure@users.noreply.github.com>
2026-05-21 15:01:54 +0700

    fix : holiday

---
+++ src/app/pages/setting-holiday/setting-holiday.ts
@@ -0,0 +1,196 @@
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { inject } from '@angular/core';
+import { TeamCalendarService } from '../../services/team-calendar.service';
+import { PageHeaderComponent } from '../../components/shared/page-header/page-header';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { EmptyStateComponent } from '../../components/shared/empty-state/empty-state';
+import { SkeletonComponent } from '../../components/shared/skeleton/skeleton';
+import { DateUtilityService } from '../../services/date-utility.service';
+import { SwalService } from '../../services/swal.service';
+
+@Component({
+  selector: 'app-setting-holiday',
+  imports: [
+    FormsModule,
+    CommonModule,
+    PageHeaderComponent,
+    NzButtonModule,
+    NzSelectModule,
+    EmptyStateComponent,
+    SkeletonComponent,
+  ],
+  templateUrl: './setting-holiday.html',
+  styleUrl: './setting-holiday.scss',
+})
+export class SettingHoliday {
+  pageTitle = signal<string>('กำหนดวันหยุด');
+
+  private teamCalendarService = inject(TeamCalendarService);
+  private swalService = inject(SwalService);
+  dateUtil = inject(DateUtilityService);
+
+  loading = signal(false);
+  years: number[] = [];
+  holidays = signal<any[]>([]);
+
+  searchYear: number | null = new Date().getFullYear();
+  searchHolidayName = '';
+
+  isHolidayModalOpen = signal(false);
+  selectedYear = new Date().getFullYear();
+  holidayFormList = signal<any[]>([]);
+
+  formData = {
+    year: 2026,
+    holidayDate: '',
+    holidayName: '',
+    description: '',
+  };
+
+  ngOnInit(): void {
+    this.generateYears();
+    this.getHoliday();
+  }
+
+  generateYears(): void {
+    const currentYear = new Date().getFullYear();
+
+    const start = currentYear - 0;
+    const end = currentYear + 5;
+
+    this.years = [];
+
+    for (let y = start; y <= end; y++) {
+      this.years.push(y);
+    }
+  }
+
+  getHoliday(): void {
+    console.log(this.searchYear, this.searchHolidayName);
+    if (!this.searchYear) {
+      this.swalService.warning('กรุณาเลือกปี');
+    }
+    this.loading.set(true);
+
+    this.teamCalendarService
+      .getHoliday(this.searchYear?.toString(), this.searchHolidayName)
+      .subscribe({
+        next: (res: any) => {
+          console.log(res);
+          this.holidays.set(this.mapHoliday(res));
+
+          this.loading.set(false);
+        },
+
+        error: (err) => {
+          console.error('Get Holiday Error : ', err);
+
+          this.loading.set(false);
+        },
+      });
+  }
+
+  private mapHoliday(res: any): any[] {
+    return res.map((x: any) => ({
+      id: x.ID,
+      year: x.YEAR,
+      holidayDate: x.HOLIDAY_DATE,
+      holidayName: x.HOLIDAY_NAME,
+    }));
+  }
+
+  addHoliday() {
+    console.log('Holiday');
+    this.isHolidayModalOpen.set(true);
+  }
+
+  addHolidayRow(): void {
+    const data = this.holidayFormList();
+
+    data.push({
+      id: crypto.randomUUID(),
+      holidayDate: '',
+      holidayName: '',
+    });
+
+    this.holidayFormList.set([...data]);
+  }
+
+  removeHolidayRow(index: number): void {
+    const data = this.holidayFormList();
+
+    data.splice(index, 1);
+
+    this.holidayFormList.set([...data]);
+  }
+
+  copyPreviousYear(): void {
+    const previousYear = this.selectedYear - 1;
+
+    this.teamCalendarService.getHoliday(previousYear.toString()).subscribe({
+      next: (res: any) => {
+        const mapped = res.map((x: any) => {
+          const oldDate = new Date(x.holidayDate);
+
+          const newDate = new Date(oldDate);
+
+          newDate.setFullYear(this.selectedYear);
+
+          return {
+            id: crypto.randomUUID(),
+            holidayDate: newDate.toISOString().split('T')[0],
+            holidayName: x.holidayName,
+          };
+        });
+
+        this.holidayFormList.set(mapped);
+      },
+
+      error: (err) => {
+        console.error(err);
+      },
+    });
+  }
+
+  saveHoliday(): void {
+    const payload = this.holidayFormList().map((x) => ({
+      year: this.selectedYear,
+      holidayDate: x.holidayDate,
+      holidayName: x.holidayName,
+    }));
+
+    console.log(payload);
+    // this.teamCalendarService.saveHoliday(payload).subscribe({
+    //   next: () => {
+    //     alert('Save Success');
+
+    //     this.closeHolidayModal();
+
+    //     this.getHoliday();
+    //   },
+
+    //   error: (err) => {
+    //     console.error(err);
+    //   },
+    // });
+  }
+
+  closeHolidayModal(): void {
+    this.isHolidayModalOpen.set(false);
+
+    this.holidayFormList.set([]);
+  }
+}
+
+interface Holiday {
+  id: number;
+  year: number;
+  holidayDate: string;
+  holidayName: string;
+  description: string;
+  createdBy: string;
+  createdDate: string;
+}
