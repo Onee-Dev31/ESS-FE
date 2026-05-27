8ab8bfa897b9f2851846fd5dedc4a8e63930ca42
pure <140476156+pure11pure@users.noreply.github.com>
2026-05-27 17:45:47 +0700

    fix : login
    - /login-v1
    - /login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,11 @@ import { authGuard } from './guards/auth-guard';
 import { menuGuard, roleGuard } from './guards/role-guard';
 import { USER_ROLES } from './constants/user-roles.constant';
 import { ValidateLoginSso } from './pages/validate/validate-login-sso/validate-login-sso';
+import { LoginVersion1 } from './pages/login-version1/login-version1';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'login-v1', component: LoginVersion1 },
   { path: 'validate/loginSSO', component: ValidateLoginSso },
   {
     path: 'qr-confirm',
