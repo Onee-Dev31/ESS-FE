651dcd501a9e057405fb87cb2150347b7209107a
pure <140476156+pure11pure@users.noreply.github.com>
2026-09-16 10:20:15 +0700

    fix : environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   api_url: '/api',
   // previewUrl: '',
-  previewUrl: 'https://ess.oneeclick.co/',
+  previewUrl: 'https://ess.oneeclick.co',
   // previewUrl: 'http://10.31.1.85:5259',
   api_ONEEJOB_url: 'https://localhost:7113/api',
   employeeImageUrl: 'https://imgemp.oneeclick.co/employeeimage',
